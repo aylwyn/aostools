588e6af16f950a72bcbf199c2e43cbac5161bac6
aylwyn <aylwyn@cantab.net>
2014-07-25 18:25:27 +0100

    use argparse subcomnds; add nohup

---
+++ submit.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# Aylwyn Scally 2014
+
+import sys
+import argparse
+import subprocess
+import os
+import os.path
+from time import strftime
+import logging
+from logging import error, warning, info, debug, critical
+#import re
+#import locale
+import string
+
+#locale.setlocale(locale.LC_ALL, 'en_GB')
+
+from aosutils import *
+
+bout_suffix = '.bout'
+outname = ''
+
+#os.umask(0002)
+
+def setbout(args):
+	if args.outname:
+		bout = args.outname + bout_suffix
+	else:
+		stardate = strftime("%y%m%d-%H%M")
+		cmdhash = str(hash(args.ARG[0]))
+		cmdstr = cmdhash[-6:] + '-' + memstr
+		bout = stardate + '-' + cmdstr + bout_suffix
+	return(bout)
+
+def checkclear(args, bout):
+	if not args.no_output:
+		if os.path.exists(args.outname) and not args.replace:
+			if not args.rerun:
+				warning('%s exists; use --replace' % args.outname)
+			sys.exit(2)
+	else:
+		if os.path.exists(bout) and not args.replace:
+			if not args.rerun:
+				warning('%s exists; use --replace' % bout)
+			sys.exit(2)
+	if not args.sim and os.path.exists(bout):
+		info('removing %s' % bout)
+		os.remove(bout)
+
+def nohup(args):
+	bout = setbout(args)
+	if args.outname and not args.no_output:
+		args.ARG += ['>', args.outname]
+	checkclear(args, bout)
+	cmd = 'nohup %s 2> %s' % (' '.join(args.ARG), bout)
+
+	info('submitting \'%s\'; output in %s' % (' '.join(args.ARG), bout))
+	subcall(cmd, args.sim)
+
+def bsub(args):
+#	if args.memG > 0.0:
+	memM = args.memG * 1e3
+	memk = args.memG * 1e6
+	args.bsub_args += '-M%d -R"select[mem>%d] rusage[mem=%d]" ' % (memM, memM, memM)
+	memstr = str(args.memG)
+	memstr += 'G'
+
+	if args.threads > 1:
+		args.bsub_args += '-n%d -R"span[hosts=1]" ' % args.threads
+
+	bout = setbout(args)
+	if args.outname and not args.no_output:
+		args.ARG += ['>', args.outname]
+	checkclear(args, bout)
+	cmd = 'bsub -o %s -G %s ' % (bout, args.grpname)
+	if args.jobname:
+		cmd += '-J"%s" ' % (args.jobname)
+	cmd += '%s "%s"' % (args.bsub_args, ' '.join(args.ARG))
+
+	info('submitting \'%s\'; output in %s' % (' '.join(args.ARG), bout))
+	subcall(cmd, args.sim, wait = True)
+
+pp = argparse.ArgumentParser(add_help=False)
+pp.add_argument('--replace', action='store_true', default = False, help = 'replace existing files')
+pp.add_argument('--rerun', action='store_true', default = False)#, help = 'rerunning')
+pp.add_argument('--sim', action='store_true', default = False, help = 'dry run')
+pp.add_argument('-v', '--verbose', action='store_true', default = False)
+pp.add_argument('--debug', action='store_true', default = False, help=argparse.SUPPRESS)
+pp.add_argument('-o', '--outname', help='output file name (used for bout file if --no_output)')
+pp.add_argument('--no_output', action='store_true', default = False, help='cmd has no output')
+pp.add_argument('ARG', nargs='*', help='command arguments to execute')
+
+p = argparse.ArgumentParser()
+s = p.add_subparsers()
+
+p1 = s.add_parser('nohup', parents=[pp], help='simple execution with nohup')#, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+p1.add_argument('-d', '--dirpref', help='directory name prefix')
+p1.add_argument('--sepvcfs', action='store_true', default = False, help='process vcfs separately (need to merge subsequently)')
+p1.set_defaults(func=nohup)
+
+p2 = s.add_parser('bsub', parents=[pp], help='submit to bsub')#, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+#p2.add_argument('DIR')
+#p2.add_argument('--parfile', default='', help='input parameter file (eigenstrat)')
+p2.add_argument('-t', '--threads', type=int, help = 'number of threads to use')
+p2.add_argument('-q', '--queue', default='normal')#, help = 'queue to use')
+p2.add_argument('-g', '--grpname', default='rdgroup')#, help = 'queue to use')
+p2.add_argument('-j', '--jobname')#, help = 'jobname to use')
+p2.add_argument('-M', '--memG', type=float, default = 2.0, help = 'GB of RAM to use')
+p2.add_argument('-A', '--bsub_args', default = '', help = 'additional bsub arguments')
+p2.set_defaults(func=bsub)
+
+args = p.parse_args()
+
+loglevel = logging.WARNING 
+if args.verbose: 
+    loglevel = logging.INFO 
+if args.debug: 
+    loglevel = logging.DEBUG 
+logging.basicConfig(format = '%(module)s:%(lineno)d:%(levelname)s: %(message)s', level = loglevel) 
+ 
+args.func(args)
+
+
